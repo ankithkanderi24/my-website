1de2c64a1ce9b9e9585f382405f16ecb025fd1a9
ankithkanderi24 <ankithkanderi@outlook.com>
2023-06-10 17:19:55 -0400

    typewriter

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,11 +3,28 @@ import './header.css'
 import Buttons from './buttons'
 import Headshot from '../../resources/headshot1copy.png'
 import Socials from './socials'
+import Typewriter from 'typewriter-effect';
+
 const header = () => {
   return (
     <header>
       <div className="container header__container">
-      <h1>I'm Ankith Kanderi!</h1>
+      <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+              .changeDelay(125)
+              .pauseFor(500)
+              .typeString("Hello!")
+              .pauseFor(1500)
+              .deleteAll()
+              .typeString("I'm Ankith Kanderi!")
+              .pauseFor(5000)
+              .callFunction(function(state) {
+                state.elements.cursor.style.color = 'transparent';
+              })
+              .start()
+            }}
+          />
       <h5 className="text-white">I'm a Senior @ Georgia Tech. </h5>
       <h5 className="text-white">I am currently recruiting for full time opportunties beginning in January 2024. </h5>   
       <Buttons />
